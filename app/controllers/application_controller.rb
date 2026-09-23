3e9e568c36f6d3c8f617e6b4be1e7ac215e7f677
Nick Budak <budak@stanford.edu>
2026-09-23 13:52:55 -0700

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  # Adds a few additional behaviors into the application controller
+  include Blacklight::Controller
+  layout :determine_layout if respond_to? :layout
+
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+        before_action :allow_geoblacklight_params
+
+        def allow_geoblacklight_params
+          # Blacklight::Parameters will pass these to params.permit
+          blacklight_config.search_state_fields.append(Geoblacklight.configuration.gbl_params)
+        end
+end
---
+++ app/controllers/catalog_controller.rb
@@ -0,0 +1,342 @@
+class CatalogController < ApplicationController
+  include Blacklight::Catalog
+
+  configure_blacklight do |config|
+    # Ensures that JSON representations of Solr Documents can be retrieved using
+    # the path /catalog/:id/raw
+    # Please see https://github.com/projectblacklight/blacklight/pull/2006/
+    config.raw_endpoint.enabled = true
+
+    gbl_config = Geoblacklight.configuration
+    field_config = gbl_config.fields
+
+    ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
+    ## @see https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html
+    ## @see https://lucene.apache.org/solr/guide/6_6/the-dismax-query-parser.html#TheDisMaxQueryParser-Theq.altParameter
+    config.default_solr_params = {
+      :start => 0,
+      "q.alt" => "*:*"
+    }
+
+    ## Default rows returned from Solr
+    ## @see https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html
+    config.default_per_page = 10
+
+    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or
+    ## parameters included in the Blacklight-jetty document requestHandler.
+    #
+    config.default_document_solr_params = {
+      qt: "document",
+      q: "{!raw f=#{field_config.id} v=$id}"
+    }
+
+    # GeoBlacklight Defaults
+    # * Adds the "map" split view for catalog#index
+    config.view.split(partials: ["index"])
+    config.view.delete_field("list")
+
+    # solr field configuration for search results/index views
+    # config.index.show_link = 'title_display'
+    # config.index.record_display_type = 'format'
+    config.index.document_component = Geoblacklight::SearchResultComponent
+    config.index.thumbnail_method = :geoblacklight_thumbnail
+    config.index.default_thumbnail = :geoblacklight_default_thumbnail
+    config.index.title_field = field_config.title
+
+    # solr field configuration for document/show views
+    # This sets the metadata to display below the map viewer.
+    # To move metadata above the map viewer,
+    # remove the lines deleting and re-adding the :show partial
+    config.show.display_type_field = "format"
+    config.show.document_component = Geoblacklight::DocumentComponent
+    config.show.sidebar_component = Geoblacklight::Document::SidebarComponent
+    config.header_component = Geoblacklight::HeaderComponent
+
+    # solr fields that will be treated as facets by the blacklight application
+    #   The ordering of the field names is the order of the display
+    #
+    # Setting a limit will trigger Blacklight's 'more' facet values link.
+    # * If left unset, then all facet values returned by solr will be displayed.
+    # * If set to an integer, then "f.somefield.facet.limit" will be added to
+    # solr request, with actual solr request being +1 your configured limit --
+    # you configure the number of items you actually want _displayed_ in a page.
+    # * If set to 'true', then no additional parameters will be sent to solr,
+    # but any 'sniffed' request limit parameters will be used for paging, with
+    # paging at requested limit -1. Can sniff from facet.limit or
+    # f.specific_field.facet.limit solr request params. This 'true' config
+    # can be used if you set limits in :default_solr_params, or as defaults
+    # on the solr side in the request handler itself. Request handler defaults
+    # sniffing requires solr requests to be made with "echoParams=all", for
+    # app code to actually have it echo'd back to see it.
+    #
+    # :show may be set to false if you don't want the facet to be drawn in the
+    # facet bar
+    # config.add_facet_field 'format', :label => 'Format'
+    # config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
+    # config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20
+    # config.add_facet_field 'language_facet', :label => 'Language', :limit => true
+    # config.add_facet_field 'lc_1letter_facet', :label => 'Call Number'
+    # config.add_facet_field 'subject_geo_facet', :label => 'Region'
+    # config.add_facet_field 'solr_bbox', :fq => "solr_bbox:IsWithin(-88,26,-79,36)", :label => 'Spatial'
+    # config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language_facet']
+
+    # config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
+    #    :years_5 => { :label => 'within 5 Years', :fq => "pub_date:[#{Time.now.year - 5 } TO *]" },
+    #    :years_10 => { :label => 'within 10 Years', :fq => "pub_date:[#{Time.now.year - 10 } TO *]" },
+    #    :years_25 => { :label => 'within 25 Years', :fq => "pub_date:[#{Time.now.year - 25 } TO *]" }
+    # }
+
+    # FACETS
+
+    # GEOBLACKLIGHT APPLICATION FACETS
+
+    # Map-Based "Search Here" Feature
+    # component:           - Defines how the facet appears in the sidebar
+    # item_presenter       - Defines how the facet values appear in the constraints section
+    # filter_query_builder - Defines the query generated for Solr
+    # filter_class         - Defines how to add/remove facet from query
+    # label                - Defines the label used for the facet
+    config.add_facet_field field_config.geometry, component: Geoblacklight::Facets::BboxFieldComponent,
+      item_presenter: Geoblacklight::BboxItemPresenter,
+      filter_class: Geoblacklight::BboxFilterField,
+      filter_query_builder: Geoblacklight::BboxFilterQuery,
+      within_boost: gbl_config.bbox_within_boost,
+      overlap_boost: gbl_config.overlap_ratio_boost,
+      overlap_field: field_config.overlap_field,
+      label: "Bounding Box"
+
+    # DEFAULT FACETS
+    config.add_facet_field field_config.resource_class, label: "Resource Class"
+    config.add_facet_field field_config.resource_type, label: "Resource Type", limit: 8
+    config.add_facet_field field_config.format, label: "Format", limit: 8
+    config.add_facet_field field_config.temporal_coverage, label: "Temporal Coverage", limit: 8
+    config.add_facet_field field_config.spatial_coverage, label: "Spatial Coverage", limit: 8, index_range: "A".."Z"
+    config.add_facet_field field_config.subject, label: "Subject", limit: 8, index_range: "A".."Z"
+    config.add_facet_field field_config.theme, label: "Theme", limit: 8, index_range: "A".."Z"
+    config.add_facet_field field_config.creator, label: "Creator", limit: 8, index_range: "A".."Z"
+    config.add_facet_field field_config.publisher, label: "Publisher", limit: 8, index_range: "A".."Z"
+    config.add_facet_field field_config.provider, label: "Provider", limit: 8
+    config.add_facet_field field_config.access_rights, label: "Access"
+    config.add_facet_field field_config.georeferenced, label: "Georeferenced"
+
+    # If you wish to add a date range filter, first run `bundle add blacklight_range_limit && bin/rails generate blacklight_range_limit:install`
+    # Then uncomment the following line:
+    # config.add_facet_field field_config.index_year, label: "Date", range: true
+
+    # Item Relationship Facets
+    # * Not displayed to end user (show: false)
+    # * Must be present for relationship "Browse all 4 records" links to work
+    # * Label value becomes the search contraint filter name
+    config.add_facet_field field_config.member_of, label: "Member Of", show: false
+    config.add_facet_field field_config.is_part_of, label: "Is Part Of", show: false
+    config.add_facet_field field_config.relation, label: "Related", show: false
+    config.add_facet_field field_config.replaces, label: "Replaces", show: false
+    config.add_facet_field field_config.is_replaced_by, label: "Is Replaced By", show: false
+    config.add_facet_field field_config.source, label: "Source", show: false
+    config.add_facet_field field_config.version, label: "Is Version Of", show: false
+
+    # Have BL send all facet field names to Solr, which has been the default
+    # previously. Simply remove these lines if you'd rather use Solr request
+    # handler defaults, or have no facets.
+    config.add_facet_fields_to_solr_request!
+
+    # ITEM VIEW FIELDS
+
+    # solr fields to be displayed in the show (single result) view
+    #  The ordering of the field names is the order of the display
+    # item_prop: [String] property given to span with Schema.org item property
+    # link_to_search: [Boolean] that can be passed to link to a facet search
+    # helper_method: [Symbol] method that can be used to render the value
+    # component: [ViewComponent::Base] component that can be used to render the field
+
+    # DEFAULT FIELDS
+    # The following fields all feature string values. If there is a value present in the metadata, they fields will show up on the item show page.
+    # The labels and order can be customized. Comment out fields to hide them.
+
+    config.add_show_field field_config.alternative_title, label: "Alternative Title",
+      itemprop: "alt_title"
+    config.add_show_field field_config.description, label: "Description", itemprop: "description",
+      component: Geoblacklight::MetadataDescriptionMarkdownComponent
+    config.add_show_field field_config.creator, label: "Creator", itemprop: "creator"
+    config.add_show_field field_config.publisher, label: "Publisher", itemprop: "publisher"
+    config.add_show_field field_config.provider, label: "Provider", link_to_facet: true
+    config.add_show_field field_config.resource_class, label: "Resource Class", itemprop: "class"
+    config.add_show_field field_config.resource_type, label: "Resource Type", itemprop: "type"
+    config.add_show_field field_config.subject, label: "Subject", itemprop: "keywords",
+      link_to_facet: true, layout_component: Geoblacklight::TruncatableMetadataFieldLayoutComponent
+    config.add_show_field field_config.theme, label: "Theme", itemprop: "theme",
+      layout_component: Geoblacklight::TruncatableMetadataFieldLayoutComponent
+    config.add_show_field field_config.temporal_coverage, label: "Temporal Coverage",
+      itemprop: "temporal"
+    config.add_show_field field_config.date_issued, label: "Date Issued", itemprop: "issued"
+    config.add_show_field field_config.spatial_coverage, label: "Spatial Coverage", itemprop: "spatial",
+      link_to_facet: true, layout_component: Geoblacklight::TruncatableMetadataFieldLayoutComponent
+    config.add_show_field field_config.rights, label: "Rights", itemprop: "rights"
+    config.add_show_field field_config.rights_holder, label: "Rights Holder",
+      itemprop: "rights_holder"
+    config.add_show_field field_config.license, label: "License", itemprop: "license"
+    config.add_show_field field_config.access_rights, label: "Access Rights",
+      itemprop: "access_rights"
+    config.add_show_field field_config.format, label: "Format", itemprop: "format"
+    config.add_show_field field_config.file_size, label: "File Size", itemprop: "file_size"
+    config.add_show_field field_config.georeferenced, label: "Georeferenced",
+      itemprop: "georeferenced"
+    config.add_show_field(
+      field_config.references,
+      label: "More details at",
+      accessor: [:external_url],
+      if: proc { |_, _, doc| doc.external_url },
+      helper_method: :render_references_url
+    )
+
+    # ADDITIONAL FIELDS
+    # The following fields are not user friendly and are not set to appear on the item show page. They contain non-literal values, codes, URIs, or are otherwise designed to power features in the interface.
+    # These values might need a translations to be readable by users.
+
+    # config.add_show_field field_config.language, label: 'Language', itemprop: 'language'
+    # config.add_show_field field_config.keyword, label: 'Keyword(s)', itemprop: 'keyword'
+
+    # config.add_show_field field_config.index_year, label: 'Year', itemprop: 'year'
+    # config.add_show_field field_config.date_range, label: 'Date Range', itemprop: 'date_range'
+
+    # config.add_show_field field_config.centroid, label: 'Centroid', itemprop: 'centroid'
+    # config.add_show_field field_config.overlap_field, label: 'Overlap BBox', itemprop: 'overlap_field'
+
+    # config.add_show_field field_config.relation, label: 'Relation', itemprop: 'relation'
+    # config.add_show_field field_config.member_of, label: 'Member Of', itemprop: 'member_of'
+    # config.add_show_field field_config.is_part_of, label: 'Is Part Of', itemprop: 'is_part_of'
+    # config.add_show_field field_config.version, label: 'Version', itemprop: 'version'
+    # config.add_show_field field_config.replaces, label: 'Replaces', itemprop: 'replaces'
+    # config.add_show_field field_config.is_replaced_by, label: 'Is Replaced By', itemprop: 'is_replaced_by'
+
+    # config.add_show_field field_config.wxs_identifier, label: 'Web Service Layer', itemprop: 'wxs_identifier'
+    # config.add_show_field field_config.id, label: 'ID', itemprop: 'id'
+    # config.add_show_field field_config.identifier, label: 'Identifier', itemprop: 'identifier'
+
+    # config.add_show_field field_config.modified, label: 'Date Modified', itemprop: 'modified'
+    # config.add_show_field field_config.metadata_version, label: 'Metadata Version', itemprop: 'metadata_version'
+    # config.add_show_field field_config.suppressed, label: 'Suppressed', itemprop: 'suppresed'
+
+    # "fielded" search configuration. Used by pulldown among other places.
+    # For supported keys in hash, see rdoc for Blacklight::SearchFields
+    #
+    # Search fields will inherit the :qt solr request handler from
+    # config[:default_solr_parameters], OR can specify a different one
+    # with a :qt key/value. Below examples inherit, except for subject
+    # that specifies the same :qt as default for our own internal
+    # testing purposes.
+    #
+    # The :key is what will be used to identify this BL search field internally,
+    # as well as in URLs -- so changing it after deployment may break bookmarked
+    # urls.  A display label will be automatically calculated from the :key,
+    # or can be specified manually to be different.
+
+    # This one uses all the defaults set by the solr request handler. Which
+    # solr request handler? The one set in config[:default_solr_parameters][:qt],
+    # since we aren't specifying it otherwise.
+
+    config.add_search_field "all_fields", label: "All Fields"
+    # config.add_search_field 'dct_title_ti', :label => 'Title'
+    # config.add_search_field 'dct_description_tmi', :label => 'Description'
+
+    # Now we see how to over-ride Solr request handler defaults, in this
+    # case for a BL "search field", which is really a dismax aggregate
+    # of Solr search fields.
+
+    # config.add_search_field('title') do |field|
+    #   # solr_parameters hash are sent to Solr as ordinary url query params.
+    #   field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+
+    #   # :solr_local_parameters will be sent using Solr LocalParams
+    #   # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+    #   # Solr parameter de-referencing like $title_qf.
+    #   # See: http://wiki.apache.org/solr/LocalParams
+    #   field.solr_local_parameters = {
+    #     :qf => '$title_qf',
+    #     :pf => '$title_pf'
+    #   }
+    # end
+
+    # config.add_search_field('author') do |field|
+    #   field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+    #   field.solr_local_parameters = {
+    #     :qf => '$author_qf',
+    #     :pf => '$author_pf'
+    #   }
+    # end
+
+    # # Specifying a :qt only to show it's possible, and so our internal automated
+    # # tests can test it. In this case it's the same as
+    # # config[:default_solr_parameters][:qt], so isn't actually neccesary.
+    # config.add_search_field('subject') do |field|
+    #   field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+    #   field.qt = 'search'
+    #   field.solr_local_parameters = {
+    #     :qf => '$subject_qf',
+    #     :pf => '$subject_pf'
+    #   }
+    # end
+
+    #  config.add_search_field('Institution') do |field|
+    #   field.solr_parameters = { :'spellcheck.dictionary' => 'Institution' }
+    #   field.solr_local_parameters = {
+    #     :qf => '$Institution_qf',
+    #     :pf => '$Institution_pf'
+    #   }
+    # end
+
+    # "sort results by" select (pulldown)
+    # label in pulldown is followed by the name of the SOLR field to sort by and
+    # whether the sort is ascending or descending (it must be asc or desc
+    # except in the relevancy case).
+    config.add_sort_field "score desc, dct_title_sort asc", label: "Relevance"
+    config.add_sort_field "#{field_config.index_year} desc, dct_title_sort asc",
+      label: "Year (Newest first)"
+    config.add_sort_field "#{field_config.index_year} asc, dct_title_sort asc",
+      label: "Year (Oldest first)"
+    config.add_sort_field "dct_title_sort asc", label: "Title (A-Z)"
+    config.add_sort_field "dct_title_sort desc", label: "Title (Z-A)"
+
+    # If there are more than this many search results, no spelling ("did you
+    # mean") suggestion is offered.
+    config.spell_max = 5
+
+    # Nav actions from Blacklight
+    config.add_nav_action(:bookmark, partial: "blacklight/nav/bookmark", if: :render_bookmarks_control?)
+    config.add_nav_action(:search_history, partial: "blacklight/nav/search_history")
+
+    # Tools from Blacklight
+    config.add_results_collection_tool(:sort_widget)
+    config.add_results_collection_tool(:per_page_widget)
+    config.add_show_tools_partial(:bookmark, partial: "bookmark_control", if: :render_bookmarks_control?)
+    config.add_show_tools_partial(:citation)
+    config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
+    config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
+
+    # Custom tools for GeoBlacklight
+    config.add_show_tools_partial :metadata, if: proc { |_context, _config, options|
+      options[:document] && (gbl_config.metadata_shown & options[:document].references.refs.map(&:type).map(&:to_s)).any?
+    }
+    config.add_show_tools_partial :arcgis, component: Geoblacklight::ArcgisComponent,
+      if: proc { |_context, _config, options| options[:document]&.arcgis_urls.present? }
+    config.add_show_tools_partial :data_dictionary, component: Geoblacklight::DataDictionaryDownloadComponent,
+      if: proc { |_context, _config, options| options[:document]&.data_dictionary_download.present? }
+    config.add_show_tools_partial :web_services, component: Geoblacklight::WebServicesLinkComponent,
+      if: proc { |_context, _config, options| options[:document] && (gbl_config.webservices_shown & options[:document].references.refs.map(&:type).map(&:to_s)).any? }
+
+    # Configuration for autocomplete suggestor
+    config.autocomplete_enabled = true
+    config.autocomplete_path = "suggest"
+  end
+
+  def web_services
+    @docs = action_documents
+
+    respond_to do |format|
+      format.html do
+        return render layout: false if request.xhr?
+        # Otherwise draw the full page
+      end
+    end
+  end
+end
